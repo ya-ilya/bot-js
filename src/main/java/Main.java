326aa95f4b6799cdcead32f23a297c9149b02658
Qther <dtaclanlegion@gmail.com>
2020-04-14 22:43:37 +0800

    Add Main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+    private final String token = ""; // TODO: Change to json
+
+
+    public static void main(String[] args) {
+        JDABuilder api = JDABuilder.createDefault(token).build();
+        
+
+    }
+}
