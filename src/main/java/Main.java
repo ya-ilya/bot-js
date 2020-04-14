890cf69ae76410dcff69c30576fcaba4c2a691cd
Qther <dtaclanlegion@gmail.com>
2020-04-14 22:44:39 +0800

    Fix Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,15 @@
 import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+
 public class Main {
-    private final String token = ""; // TODO: Change to json
+    private static final String token = ""; // TODO: Change to json
 
 
-    public static void main(String[] args) {
-        JDABuilder api = JDABuilder.createDefault(token).build();
+    public static void main(String[] args) throws LoginException {
+        JDA api = JDABuilder.createDefault(token).build();
         
 
     }
