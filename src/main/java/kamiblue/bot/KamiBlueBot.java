36244f84929c1038bd2b0da3dc8a0a15e27a826e
Qther <dtaclanlegion@gmail.com>
2020-04-16 22:36:47 +0800

    Fix Listener and add Suggestion Reactor

--- src/main/java/kamiblue/bot/KamiBlueBot.java
+++ src/main/java/kamiblue/bot/KamiBlueBot.java
@@ -7,31 +7,33 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
 
-public class KamiBlueBot {
+public class KamiBlueBot extends ListenerAdapter {
     public static JDA api;
 
     public static void main(String[] args) throws LoginException {
         Dotenv dotenv = Dotenv.load();
         String token = dotenv.get("TOKEN");
 
-        api = JDABuilder.createDefault(token).build();
+        api = JDABuilder.createDefault(token).addEventListeners(new KamiBlueBot()).build();
+
     }
 
-    public class MyListener extends ListenerAdapter {
-        @Override
-        public void onMessageReceived(MessageReceivedEvent event)
-        {
-            if (event.getAuthor().isBot()) return;
-            Message message = event.getMessage();
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+        Message message = event.getMessage();
+        MessageChannel channel = event.getChannel();
+        if (message.getContentRaw().startsWith(Command.getCommandPrefix())) {
             String content = message.getContentRaw().substring(0, Command.prefix.length() + 1);
-            MessageChannel channel = event.getChannel();
             CommandManager.callCommand(content, channel);
+        } else if (channel.getId().equalsIgnoreCase("634010802403409931") || channel.getId().equalsIgnoreCase("665638113732984872")) {
+            message.addReaction("\uD83D\uDC4D").queue();
+            message.addReaction("\uD83D\uDC4E").queue();
         }
     }
-
 }
