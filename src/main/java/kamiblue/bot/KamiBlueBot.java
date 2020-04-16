af5e5a477b1888eb30eecf8d7d02b628bbfdd70f
mattymatty97 <mattymatty97@users.noreply.github.com>
2020-04-16 19:04:03 +0200

    fixes and improvements also added a Exception printer to Embed to see the errors

--- src/main/java/kamiblue/bot/KamiBlueBot.java
+++ src/main/java/kamiblue/bot/KamiBlueBot.java
@@ -1,8 +1,11 @@
 package kamiblue.bot;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import jdk.internal.joptsimple.internal.Strings;
 import kamiblue.bot.command.Command;
 import kamiblue.bot.command.CommandManager;
+import kamiblue.bot.utils.EmbedType;
+import kamiblue.bot.utils.KamiBotUtils;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Message;
@@ -11,6 +14,8 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.security.auth.login.LoginException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 public class KamiBlueBot extends ListenerAdapter {
     public static JDA api;
@@ -33,7 +38,14 @@ public class KamiBlueBot extends ListenerAdapter {
         MessageChannel channel = event.getChannel();
         if (message.getContentRaw().startsWith(Command.prefix)) {
             String content = message.getContentRaw().substring(0, Command.prefix.length() + 1);
-            CommandManager.callCommand(content, channel);
+            try {
+                CommandManager.callCommand(content, channel);
+            }catch (Exception ex){
+                StringWriter sw = new StringWriter();
+                ex.printStackTrace(new PrintWriter(sw));
+                String exceptionAsString = sw.toString();
+                channel.sendMessage(KamiBotUtils.generateEmbedResponse(EmbedType.ERROR,"Exception in code",exceptionAsString)).queue();
+            }
         } else if (channel.getId().equalsIgnoreCase("634010802403409931") || channel.getId().equalsIgnoreCase("665638113732984872")) {
             message.addReaction("\uD83D\uDC4D").queue();
             message.addReaction("\uD83D\uDC4E").queue();
