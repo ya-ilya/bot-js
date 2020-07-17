66b19e0fe1c73459f15d32a2824b272c6671b361
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-17 19:38:43 -0400

    updated it with embed: please work ;update command!

--- commands/version.js
+++ commands/version.js
@@ -1,35 +1,42 @@
 const Discord = require("discord.js");
 const fs = require("graceful-fs");
+const fetch = require('node-fetch');
 const exec = require('child_process').exec;
 
 module.exports.run = async (client, message, args) => {
-if (!(message.author.id === "563138570953687061") && !(message.author.id === "297096161842429963")) return;
-message.reply("sure")
-exec('git rev-parse `git branch -r --sort=committerdate | tail -1`', (error, stdout, stderr) => {
-  if (error) {
-    message.channel.send(`exec error: ${error}`);
-    return;
-  }
-  message.channel.send(`latest: ${stdout}`);
-  message.channel.send(`stderr: ${stderr}`);
-});
+    if (!(message.author.id === "563138570953687061") && !(message.author.id === "297096161842429963")) return;
+    message.reply("Fetching version info...")
 
-exec('git rev-parse --verify HEAD', (error, stdout, stderr) => {
-  if (error) {
-    message.channel.send(`exec error: ${error}`);
-    return;
-  }
-  message.channel.send(`local: ${stdout}`);
-  message.channel.send(`stderr: ${stderr}`);
-});
-  
-} 
+    exec('git rev-parse --verify HEAD', (error, stdout, stderr) => {
+        if (error) {
+            console.log(`exec error: ${error}`);
+            return;
+        }
+        fetch(`https://api.github.com/repos/kami-blue/bot/commits/master`).then(res => res.json()).then((out) => {
+            var latest_sha = out.sha;
+            var local_sha = stdout;
+            let embedTitle = "Versions Differ";
+            if (local_sha === latest_sha) embedTitle = "All up to date!!";
+
+            let versionEmbed = new Discord.MessageEmbed()
+                .setTitle(embedTitle)
+                .setColor(client.colors.kamiblue)
+                .setDescription(`**Latest Sha:** ${latest_sha}\n**Local Sha:** ${local_sha}`);
+            message.channel.send(versionEmbed);
+
+        }).catch(err => {
+            console.log(err);
+        });
+
+    });
+
+}
 
 module.exports.config = {
-  name: "version",
-  aliases: [],
-  use: "version",
-  description: "versione!!!",
-  state : "gamma",
-  page: 4
+    name: "version",
+    aliases: [],
+    use: "version",
+    description: "version checker!!!",
+    state: "gamma",
+    page: 4
 };
