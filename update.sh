4a6e504a47c58d31db8391bd4e2a6391fc7a8389
Dominika <sokolov.dominika@gmail.com>
2020-07-17 14:52:42 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-WEBHOOK_URL="https://discordapp.com/api/webhooks/733754791636893816/OmucpgEk-cAXOhnUGzQhrLqffoQ5j7JlNSSMnaZFywlnyapcPMrlzsitwyXCG5eZdz-3"
-JSON="{\"content\": \"test\"}"
-
-curl -d "$JSON" -H "Content-Type: application/json" "$WEBHOOK_URL"
+pkill -9 node
+./home/mika/bot/run.sh
