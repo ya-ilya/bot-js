4c4aa8f31aebf108c9662479cb358e309ca0caae
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-17 11:35:02 -0400

    fiex

--- commands/issue.js
+++ commands/issue.js
@@ -9,7 +9,7 @@ module.exports.run = async (client, message, args) => {
   fetch("https://api.github.com/repos/kami-blue/" + args[0] + "/issues?page=0&per_page=1").then(res => res.json()).then((out) => {
     console.log(out.length)
     
-    if ((args[1] == args[1]) && (args[1] <= out.length) && !!args[1]) {
+    if ((args[1] == args[1]) && (args[1] > out.length - 1) && !!args[1]) {
       message.channel.send("**Here's the issue!**\nhttps://github.com/kami-blue/" + args[0] + "/issues/" + args[1]);
     } else {
       message.channel.send("**Invalid Issue!**\nkami-blue/" + args[0] + "/issues/" + args[1] + " does not exist");
