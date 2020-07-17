0ee8a860ac7e2bc5293c515fb8b63411ae3bf9b1
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-17 15:58:31 -0400

    Create version.js

---
+++ commands/version.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+const exec = require('child_process').exec;
+
+module.exports.run = async (client, message, args) => {
+if (!(message.author.id === "563138570953687061") && !(message.author.id === "297096161842429963")) return;
+message.reply("sure")
+exec('git rev-parse `git branch -r --sort=committerdate | tail -1`', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  message.channel.send(`latest: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
+
+exec('git ls-remote --sort=committerdate`', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  message.channel.send(`local: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
+  
+} 
+
+module.exports.config = {
+  name: "version",
+  aliases: [],
+  use: "version",
+  description: "versione!!!",
+  state : "gamma",
+  page: 4
+};
