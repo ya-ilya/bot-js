aa68b90493be6e337a8192bf60d6f653a7a89338
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-17 13:28:25 -0400

    removed code 4 now

--- index.js
+++ index.js
@@ -50,15 +50,14 @@ client.on("ready", () => {
     const activities_list = ["You skid KAMI"]; // add more then add the type of them below
     const activities_type = ["WATCHING"]; // types are PLAYING WATCHING LISTENING and STREAMING
     console.log("Bot online");
-    setInterval(() => {
-        const activityIndex = Math.floor(Math.random() * (activities_list.length - 1) + 1);
-        client.user.setPresence({
-            activity: {
-                name: activities_list[activityIndex],
-                type: activities_type[activityIndex]
-            }
-        });
-    }, 1e4); // Ten Seconds
+    const activityIndex = Math.floor(Math.random() * (activities_list.length - 1) + 1);
+    client.user.setPresence({
+        activity: {
+            name: activities_list[activityIndex],
+            type: activities_type[activityIndex]
+        }
+    });
+
 });
 
 fs.readdir("./commands/", (err, files) => {
