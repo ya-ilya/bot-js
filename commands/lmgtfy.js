a87726b17dddf122ab31c6590718ba17426961e9
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-17 11:45:15 -0400

    Solves
    all questions

---
+++ commands/lmgtfy.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+module.exports.run = async (client, message, args) => {
+  let lmgtfyEmbed = new Discord.MessageEmbed()
+    .setTitle("Here you go~!")
+    .setDescription(`[Solution](https://lmgtfy.com/?q=${args.join("+")})`)
+    .setColor(client.colors.kamiblue);
+  message.channel.send(lmgtfyEmbed);
+};
+
+module.exports.config = {
+  name: "lmgtfy",
+  aliases: [],
+  use: "lmgtfy [Query]",
+  description: "Solves all questions (magic)",
+  state: "gamma",
+  page: 1
+};
