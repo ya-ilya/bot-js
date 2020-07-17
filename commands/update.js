c6665dfc8bcca84b75e73a721a9da62ffebaf3f8
Dominika <sokolov.dominika@gmail.com>
2020-07-17 14:41:35 -0400

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
 const exec = require('child_process').exec, child;
 const myShellScript = exec('sh update.sh /home/mika/bot/');
 
@@ -9,3 +11,16 @@ myShellScript.stdout.on('data', (data)=>{
 myShellScript.stderr.on('data', (data)=>{
     console.error(data);
 });
+
+module.exports.run = async (client, message, args) => {
+  message.channel.send("Updated!")
+};
+
+module.exports.config = {
+  name: "update",
+  aliases: [],
+  use: "update",
+  description: "Update the bot",
+  state: "gamma",
+  page: 1
+};
