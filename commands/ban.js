c6cdc8fc369ff16864740843fb080d72bf231ce9
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-17 22:29:50 -0400

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+
+module.exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("How would you like it if I banned **you**!");
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+    if (!member) return message.channel.send("Please mention a valid user.");
+    if (!member.bannable) return message.channel.send("This member cannot be banned?");
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No Reason Provided";
+    await member.ban(reason).catch(error => message.channel.send(error));
+    let banEmbed = new Discord.MessageEmbed()
+      .setTitle("User Banned")
+      .setDescription(`${member.user.tag} has been banned by ${message.author.tag}`)
+      .addField("Reason", reason)
+      .setColor("ff5050");
+    message.channel.send(banEmbed);
+} 
+
+module.exports.config = {
+  name: "ban",
+  aliases: ["execute", "exile", "glean"],
+  use: "ban [@User]",
+  description: "Ban a user from the server",
+  state : "gamma",
+  page: 4
+};
