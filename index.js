c9477a634fd5952cc790059f1c4b568dfabe873d
Dominika <sokolov.dominika@gmail.com>
2020-07-19 11:29:10 -0400

    close #31

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ client.on('message', async message => {
 
     if (!message.member.hasPermission("CHANGE_NICKNAME")) {
         var discordInvite = new RegExp("(d.{0,3}i.{0,3}s.{0,3}c.{0,3}o.{0,3}r.{0,3}d).{0,7}(gg|com.{0,3}invite)");
-        var hacksRegex = new RegExp("(ch.{0,2}t|h.{0,2}k.{0,1})");
+        var hacksRegex = new RegExp("(ch(?!es).{0,2}t|h(?!in|un|an).{0,2}k)");
         if (hacksRegex.test(message.content.toLowerCase())) {
             message.channel.send("Hacks / cheats are against Discord TOS (Rules 3 and 9)");
         } else if (discordInvite.test(message.content.toLowerCase())) {
