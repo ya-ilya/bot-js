329d8ae815f570d913c487ecc5bf4fbf1da5cf22
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-19 11:24:04 -0400

    Done (ez)

--- commands/regex.js
+++ commands/regex.js
@@ -2,16 +2,15 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-   let regArgs = args.join(" ").split("~");
+   let regArgs = args.join(" ").split("#");
    var regEz = new RegExp(regArgs[1]);
-   if (regEz.exec(regArgs[0])) return message.channel.send("true");
-   return message.channel.send("false");
+   return message.channel.send((regEz.exec(regArgs[0])));
 } 
 
 module.exports.config = {
   name: "regex",
   aliases: [],
-  use: "regex [String~Regex]",
+  use: "regex [String#Regex]",
   description: "Tests 4 regex",
   state : "gamma",
   page: 1
