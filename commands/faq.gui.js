fb7291bbea2f0e3fa8afab7513701a75879257f4
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-18 21:21:57 -0400

    Create faq.gui.js
    When Galacta Knight is defeated in Meta Knightmare Ultra and The True Arena, he flickers white and flies off into space. In Meta Knightmare Ultra, a short cutscene plays, where Galacta Knight is shown flying unstably in space, then rapidly appearing in multiple parts of the screen, as if he was exploding. He then appears in the middle of the screen where he is engulfed in a bright, white flash. Galacta Knight's wings are torn off his body, while his sword and shield is shown leaving him as well. The disarmed knight then vanishes in the flash, while Meta Knight returns to Planet Popstar. In The True Arena, this cutscene is not shown, as Kirby now has to fight Marx Soul in the final round

---
+++ commands/faq.gui.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+module.exports.run = async (client, message, args) => {
+  message.channel.send("You can make your GUI smaller by going to Options -> Video Settings and changing GUI Scale to something like Normal or Small\n\nUse ;fixgui in game if you're missing any menus after this ")
+};
+
+module.exports.config = {
+  name: "gui",
+  aliases: [],
+  use: "gui",
+  description: "How to use the GUI on KAMI Blue",
+  state: "gamma",
+  page: 2
+};
