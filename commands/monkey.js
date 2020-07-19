ca24a23055d07ee5d1932b2ea6321cff4167164f
Andres091 <68240138+Andres091@users.noreply.github.com>
2020-07-19 14:57:22 -0400

    Create monkey.js
    Funny monkey that helps you convey an emotion that cannot be shown in any other ways

---
+++ commands/monkey.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+module.exports.run = async (client, message, args) => {
+  
+ var Monkeys = ["🐵","🐒","🦧","🐵 🐒 🦧","🙈","🙊","🙉","🙈🙊🙉"];
+    message.channel.send(Monkeys[Math.floor(Math.random() * 8)])
+  
+} 
+
+module.exports.config = {
+  name: "farmerjoe",
+  aliases: [],
+  use: "farmerjoe",
+  description: "funny monkey",
+  state : "gamma",
+  page: 1
+};
