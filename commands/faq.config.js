887373a9c84fa50eb170f246da3fe5fcec4aa1f6
Dominika <sokolov.dominika@gmail.com>
2020-07-20 15:59:14 -0400

    the funny

--- commands/faq.config.js
+++ commands/faq.config.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send("Why the hell do people keep asking for settings. Literally just configure it on your own.\n\nI guarantee you that my binds and macros won't make any sense. Do you have baritone stop bound to `o`? probably not. \n\nDo you have crystalaura bound to `f8`? You honestly probably don't and it probably wouldn't make sense to you. \n\nJust configure it to your personal preferences!")
+  if (args[0] == "save") {
+      message.channel.send("Saved configuration and macros!")
+  }
+  else {
+      message.channel.send("Why the hell do people keep asking for settings. Literally just configure it on your own.\n\nI guarantee you that my binds and macros won't make any sense. Do you have baritone stop bound to `o`? probably not. \n\nDo you have crystalaura bound to `f8`? You honestly probably don't and it probably wouldn't make sense to you. \n\nJust configure it to your personal preferences!")
+  }
 };
 
 module.exports.config = {
