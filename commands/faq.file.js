3b4e1d85a07336c0c481cf825d2a76802ab2406e
Dominika <sokolov.dominika@gmail.com>
2020-07-20 12:59:51 -0400

    translated ;file

--- commands/faq.file.js
+++ commands/faq.file.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send("By default, Windows hides file extensions. Make a text file without the .txt at the end, as it's automatically added and hidden.")
+  if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
+      message.channel.send("По умолчанию Windows скрывает расширения файлов. Создайте текстовый файл без .txt в конце, так как он автоматически добавляется и скрывается.")
+  }
+  else {
+      message.channel.send("By default, Windows hides file extensions. Make a text file without the .txt at the end, as it's automatically added and hidden.")
+  }
 };
 
 module.exports.config = {
