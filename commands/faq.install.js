bf44c0f06ec540188d192f3778ece16ae9217296
Dominika <sokolov.dominika@gmail.com>
2020-07-20 12:58:14 -0400

    translated ;install

--- commands/faq.install.js
+++ commands/faq.install.js
@@ -2,9 +2,14 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send("Download this from <#634549110145286156> or from the website at https://kamiblue.org/download\n\nThis is a 1.12.2 Forge mod. You can open the jar to install it, or manually move the jar to your mods folder.")
+  if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
+      message.channel.send("KAMI Blue можно скачать с <#634549110145286156> или с нашего сайта https://kamiblue.org/download\n\nЭто мод 1.12.2 Forge. чтобы установить её вручную переместите .jar в папку с модами.")
+  }
+  else {
+      message.channel.send("Download this from <#634549110145286156> or from the website at https://kamiblue.org/download\n\nThis is a 1.12.2 Forge mod. You can open the jar to install it, or manually move the jar to your mods folder.")
+  }
 };
-
+  
 module.exports.config = {
   name: "install",
   aliases: [],
