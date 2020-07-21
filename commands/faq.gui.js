639ba9cf5ac71ce9955d865f7732fda918b96ce7
Dominika <sokolov.dominika@gmail.com>
2020-07-21 09:12:32 -0400

    update Russian gui faq (#56)

--- commands/faq.gui.js
+++ commands/faq.gui.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
   if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
-      message.channel.send("Вы можете уменьшить графический интерфейс, перейдя в «Настройки» -> «Настройки графики» и изменив масштаб графического интерфейса на «Обычный» или «Маленький».\n\nИспользуйте команду ;fixgui в игре, если после это всё ровно у вас маленькое меню")
+      messafe.channel.send("Вы можете уменьшить графический интерфейс, перейдя в «Настройки» -> «Настройки графики» и изменив масштаб графического интерфейса на «Обычный» или «Маленький».\n\nИспользуйте команду ;fixgui в игре, если после это всё ровно у вас маленькое меню или можете включить язык English (US)")
   }
   else {
       message.channel.send("You can make your GUI smaller by going to Options -> Video Settings and changing GUI Scale to something like Normal or Small\n\nUse ;fixgui in game if you're missing any menus after this ")
