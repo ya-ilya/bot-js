45ded574a2cfef6139de4bc31484317a13653089
Dominika <sokolov.dominika@gmail.com>
2020-07-21 09:48:16 -0400

    translated shulkerpreview to russian

--- commands/faq.shulkerpreview.js
+++ commands/faq.shulkerpreview.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send("2b2t completely disabled shulker packets unless you actually right click the shulker, meaning there is no way to bypass that, and no way to preview shulkers on 2b.\nThis is the case in all clients, not just KAMI Blue, because it also patches the vanilla text that has `64x - Obsidian` in the vanilla tooltip. **There is no bypass.**")
+  if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
+      message.channel.send("2b2t полностью отключил пакеты shulker, если вы фактически не нажмете правой кнопкой мыши на shulker, то есть нет способа обойти это, и нет способа предварительного просмотра shulker на 2b.\nЭто относится ко всем клиентам, не только к KAMI Blue, потому что он также исправляет ванильный текст с `64х Обсидианом` в подсказке vanilla. Обхода нет.")
+  }
+  else {
+      message.channel.send("2b2t completely disabled shulker packets unless you actually right click the shulker, meaning there is no way to bypass that, and no way to preview shulkers on 2b.\nThis is the case in all clients, not just KAMI Blue, because it also patches the vanilla text that has `64x - Obsidian` in the vanilla tooltip. **There is no bypass.**")
+  }
 };
 
 module.exports.config = {
