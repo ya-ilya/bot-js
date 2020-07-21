423bb6009f01bcfd875265803f900bb709d1dc49
IcyChungus <47074495+sourTaste000@users.noreply.github.com>
2020-07-21 16:20:56 -0700

    guys i did it (#60)
    * success is near
    
    * Guys i did it

--- commands/serverstat.js
+++ commands/serverstat.js
@@ -1,24 +1,30 @@
-const {ServerStats} = require("minestats");
+const fetch = require('node-fetch');
 const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     try {
-        ServerStats(args[0]).then(info => {
-            let final = JSON.parse(info);
-            let statusEmbed = new Discord.MessageEmbed()
-                .setTitle(`Minecraft server status of ${args[0]}: `)
-                .setDescription(`
-                    Message Of The Day: ${final.motd}
-                    Online Players: ${final.players.now}
-                    Max Players: ${final.players.max}
-                    Version: ${final.server.name}
-                `)
-                .setColor(client.colors.kamiblue)
-            message.channel.send(statusEmbed);
-        })
+        let result;
+        fetch(`https://mcapi.us/server/status?ip=${args[0]}`)
+            .then(response => response.json())
+            .then(data => {
+                result = JSON.parse(JSON.stringify(data));
+                let statusEmbed = new Discord.MessageEmbed()
+                    .setTitle(`Server Status of ${args[0]}: `)
+                    .setDescription(`
+                    Server online? ${result.online}
+                    Message of the day: ${result.motd}
+                    Players online: ${result.players.now}
+                    Max Players online: ${result.players.max}
+                    Server version: ${result.server.name}
+                    `)
+                    .setColor(client.colors.kamiblue)
+                message.channel.send(statusEmbed);
+            })
+            .catch(err => console.error(err))
+
     }catch(err){
-        message.channel.send("Failed to get the info of server! Is server online?");
+        message.channel.send("Failed to get the info of server!");
         console.error(err);
     }
 }
--- commands/uuid.js
+++ commands/uuid.js
@@ -3,20 +3,26 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-    try {
-        const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`);
-        const result = await JSON.parse(response);
+        try {
+            let result;
+            fetch(`https://api.mojang.com/users/profiles/minecraft/${args[0]}`)
+                .then(response => response.json())
+                .then(data => {
+                    result = JSON.parse(JSON.stringify(data));
+                    console.log(result)
+                })
+                .catch(err => console.error(err))
 
-        let uuidEmbed = new Discord.MessageEmbed()
-                .setTitle(`UUID of player ${result.name}: `)
+            let uuidEmbed = new Discord.MessageEmbed()
+                .setTitle('UUID of player ' + result)
                 .setDescription(result.id)
                 .setColor(client.colors.kamiblue);
 
-        message.channel.send(uuidEmbed);
-    }catch(err){
-        message.channel.send("Failed to get the uuid of player!");
-        console.error(err);
-    }
+            message.channel.send(uuidEmbed);
+        } catch (err) {
+            message.channel.send("Failed to get the uuid of player!");
+            console.error(err);
+        }
 }
 
 module.exports.config = {
