c1ffecbcb7923cec2f22963852ad4ba0ff7828bd
Dominika <sokolov.dominika@gmail.com>
2020-07-21 09:24:12 -0400

    translated set to russian

--- commands/faq.set.js
+++ commands/faq.set.js
@@ -2,7 +2,12 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-  message.channel.send("Use `;set module setting value` to manually change the value of a setting. Eg `;set elytraflight speedc 2.3999999` or `;customchat \"ur ez\"`. For single arguments with spaces, put \"\" around the argument, \"like so\"")
+  if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
+      message.channel.send("Используйте `;set module setting value` чтобы вручную поменять знание функции. Например `;set elytraflight speedc 2.3999999` или `;customchat \"ur ez\"`. Для одиночных аргументов с пробелами, поставьте вокруг аргумента \"\", \"вот так\"")
+  }
+  else {
+      message.channel.send("Use `;set module setting value` to manually change the value of a setting. Eg `;set elytraflight speedc 2.3999999` or `;customchat \"ur ez\"`. For single arguments with spaces, put \"\" around the argument, \"like so\"")
+  }
 };
 
 module.exports.config = {
