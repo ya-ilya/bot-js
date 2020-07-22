39b2622dee0d49387ac4db425725040a81929156
Dominika <sokolov.dominika@gmail.com>
2020-07-22 16:35:57 -0400

    Lmgtfy bug fixing 2 electric bungalow: JS Moment

--- commands/lmgtfy.js
+++ commands/lmgtfy.js
@@ -2,7 +2,10 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send("\"" + args[0] + "\"")
+    if (args[0] === "undefined") {
+        message.channel.send("Please enter a question query!");
+        return;
+    }
 
     let lmgtfyEmbed = new Discord.MessageEmbed()
         .setTitle("Here you go~!")
