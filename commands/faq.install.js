ea6cdb2552831233bb9c4084a557a571ba0b798a
Dominika <sokolov.dominika@gmail.com>
2020-07-21 23:49:09 -0400

    Update russian translation in install

--- commands/faq.install.js
+++ commands/faq.install.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
   if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
-      message.channel.send("KAMI Blue можно скачать с <#634549110145286156> или с нашего сайта https://kamiblue.org/download\n\nЭто мод 1.12.2 Forge. чтобы установить её вручную переместите .jar в папку с модами.")
+      message.channel.send("KAMI Blue можно скачать с <#634549110145286156> или с нашего сайта https://kamiblue.org/download\n\nЭтот мод на версию 1.12.2. Переместите мод в папку mods `.minecraft/mods` когда вы переместили мод в папку mods вы должны запустить Fogre 1.12.2 (Forge устанавливается на лицензионном лаунчере отдельно)")
   }
   else {
       message.channel.send("Download this from <#634549110145286156> or from the website at https://kamiblue.org/download\n\nThis is a 1.12.2 Forge mod. You can open the jar to install it, or manually move the jar to your mods folder.")
