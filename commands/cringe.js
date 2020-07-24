5c60849586dc1419fdfc930a75fd7cd97aa91310
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-07-23 21:41:45 -0400

    Update cringe.js

--- commands/cringe.js
+++ commands/cringe.js
@@ -29,7 +29,7 @@ module.exports.run = async (client, message, args) => {
 
             ctx.font = `25px Whitney\\-Medium`;
             ctx.fillStyle = '#ffffff';
-            ctx.fillText(fetchedMessage.author.name, 100, (messages.indexOf(fetchedMessage) * 100) + 40);
+            ctx.fillText((fetchedMessage.author.name).replace("カミブルー！", "KAMI Blue"), 100, (messages.indexOf(fetchedMessage) * 100) + 40);
 
             ctx.font = `25px Whitney\\-Medium`;
             ctx.fillStyle = '#ffffff';
