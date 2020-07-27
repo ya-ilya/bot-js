97dd8ac979993fe35a14ff61a751f87c4cab1f05
Dominika <sokolov.dominika@gmail.com>
2020-07-27 19:16:26 -0400

    Added avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+module.exports.run = async (client, message, args) => {
+    let embed = new Discord.MessageEmbed()
+        .setTitle(message.author.username)
+        .setImage(message.author.avatar)
+    message.channel.send(embed)
+}
+
+module.exports.config = {
+    name: "avatar",
+    aliases: ["av"],
+    use: "avatar [@User]",
+    description: "Sends the users profile picture as a larger image",
+    state: "gamma",
+    page: 1
+};
