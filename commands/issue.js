6ef9a797935771d6f245eedb0643e394c8a36e74
IcyChungus <47074495+sourTaste000@users.noreply.github.com>
2020-07-29 15:34:15 -0700

    fixed issue command (#107)

--- commands/issue.js
+++ commands/issue.js
@@ -8,25 +8,29 @@ module.exports.run = async (client, message, args) => {
     try {
         let result;
         let milestone;
+        let assignee;
         fetch(`https://api.github.com/repos/kami-blue/${args[0]}/issues/${args[1]}`)
             .then(response => response.json())
             .then(data => {
                 result = JSON.parse(JSON.stringify(data));
                 try{if(!result.milestone.title == null){milestone = result.milestone.title}}catch (err){milestone = "None"}
-                let issueEmbed = new Discord.MessageEmbed()
-                    .setAuthor("カミブルー！", "https://cdn.discordapp.com/avatars/638403216278683661/1e8bed04cb18e1cb1239e208a01893a1.png", "https://kamiblue.org")
-                    .setTitle(result.title)
-                    .setURL(result.html_url)
-                    .setThumbnail(result.user.avatar_url)
-                    .setDescription(result.body)
-                    //.addField("Assignee", `${result.assignee.login}`)
-                    //someone fix these
-                    .addField("Labels", result.labels.forEach(length => length))
-                    //.addField("Assignee", result.assignees.forEach(assignees => assignees))
+                try{if(!result.assignee.login == null){assignee = result.assignee.login}}catch (err){assignee = "None"}
+                try {
+                    let issueEmbed = new Discord.MessageEmbed()
+                        .setAuthor("カミブルー！", "https://cdn.discordapp.com/avatars/638403216278683661/1e8bed04cb18e1cb1239e208a01893a1.png", "https://kamiblue.org")
+                        .setTitle(result.title)
+                        .setURL(result.html_url)
+                        .setThumbnail(result.user.avatar_url)
+                        .setDescription(result.body)
+                        .addField("Labels", result.labels[0].name)
+                        .addField("Assignee", assignee)
+                        .addField("Milestone", milestone)
+                        .setColor(client.colors.kamiblue)
 
-                    .addField("Milestone", milestone)
-                    .setColor(client.colors.kamiblue)
-                message.channel.send(issueEmbed)
+                    message.channel.send(issueEmbed)
+                }catch (e) {
+                    message.channel.send("Bad issue number or repository!");
+                }
             })
     } catch (e) {
         message.channel.send("*Here's the issue!*\nCould not confirm validity of issue\nhttps://github.com/kami-blue/" + args[0] + "/issues/" + args[1]);
