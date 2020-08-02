b77edef859e942f2fb1257448d794df14e86c0eb
Dominika <sokolov.dominika@gmail.com>
2020-08-02 18:39:53 -0400

    update Russian translation of mcrash

--- commands/faq.macrocrash.js
+++ commands/faq.macrocrash.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
-        message.channel.send("Удалите файл `KAMIBlueMacros` в `.minecraft`, это известная ошибка, которую мы еще не исправили")
+        message.channel.send("Удалите файл `KAMIBlueMacros` в `.minecraft`, Эта ошибка уже исправлена ​​в последней бета-версии, но вам все равно нужно удалить файл после обновления")
     } else {
         message.channel.send("Delete the `KAMIBlueMacros` file in `.minecraft`. This bug is already fixed in the latest beta, but you still need to delete the file after updating")
     }
