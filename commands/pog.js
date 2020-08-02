3fff7e137dcb540ca1312f32211bfe2012aecbbb
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-08-01 20:40:22 -0400

    just fixing this to keep my streak

--- commands/pog.js
+++ commands/pog.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
 
-    message.channel.send(`pog`);
+    message.channel.send(`Pog <:catsunglasses:704106255492579438>`);
 
 }
 
