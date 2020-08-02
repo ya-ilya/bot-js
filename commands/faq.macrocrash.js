0c35b2f481746f6470e567cb426755ed00696413
Dominika <sokolov.dominika@gmail.com>
2020-08-02 13:36:15 -0400

    Update faq.macrocrash.js

--- commands/faq.macrocrash.js
+++ commands/faq.macrocrash.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
         message.channel.send("Удалите файл `KAMIBlueMacros` в `.minecraft`, это известная ошибка, которую мы еще не исправили")
     } else {
-        message.channel.send("Delete the `KAMIBlueMacros` file in `.minecraft`, that's a known bug which hasn't been fixed yet")
+        message.channel.send("Delete the `KAMIBlueMacros` file in `.minecraft`. This bug is already fixed in the latest beta, but you still need to delete the file after updating")
     }
 };
 
