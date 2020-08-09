8af757b1e5a2802188fffdf2bf7f9a94c188012e
Dominika <sokolov.dominika@gmail.com>
2020-08-09 18:41:06 -0400

    updated elytra help for MinTakeoffHeight

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ function autoResponder(message) {
         if (elytraRegex2.test(cleanedMessage)) elytraRegexMatches++;
 
         if (elytraRegexMatches > 1 && elytraMatch) {
-            return message.channel.send("Make sure you're using default settings in the latest beta. Run the defaults button in ElytraFlight's settings if you updated KAMI Blue before.\n\nIf it still doesn't help, make sure you're not using NoFall, AntiHunger or any other movement related mods from **other** clients, such as Sprint in Rage mode, as they make you go over the speed limit and rubberband.");
+            return message.channel.send("Make sure you're using default settings in the latest beta. Run the defaults button in ElytraFlight's settings if you updated KAMI Blue before.\n\nIf it still doesn't help, make sure you're not using NoFall, AntiHunger or any other movement related mods from **other** clients, such as Sprint in Rage mode, as they make you go over the speed limit and rubberband.\n\nIf you're having issues taking off at higher ping on 2b2t, lower the MinTakeoffHeight setting");
         }
 
         /* game crash regex */
