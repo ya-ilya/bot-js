880100d8e318b532e1e3e6d176ab72faf0f804d7
Dominika <sokolov.dominika@gmail.com>
2020-08-10 13:53:13 -0400

    fix crash

--- commands/version.js
+++ commands/version.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 const fetch = require('node-fetch');
 const exec = require('child_process').exec;
+const auth = require("../auth.json");
 
 module.exports.run = async (client, message, args) => {
     if (!(message.author.id === "563138570953687061") && !(message.author.id === "297096161842429963")) return;
