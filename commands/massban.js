3ecce9f4ebc7220bd3d9e3e17a0df3c016ba2d61
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-08-13 15:31:44 -0400

    Update massban.js

--- commands/massban.js
+++ commands/massban.js
@@ -7,10 +7,10 @@ module.exports.run = async (client, message, args) => {
     if (!args[0]) return message.channel.send("Specifiy ids to ban");
     let reason = "Get ezed";
     args.forEach(async user => {
+        console.log(user)
         var member = client.users.cache.get(user);
-        message.channel.send(member.username + "was banend")
         if (member.hasPermission("BAN_MEMBERS")) return message.channel.send("How would you like it if I banned **you**!");
-        await member.send("You were banned");
+        message.channel.send(member.username + "was banend")
         await member.ban(reason).catch(error => message.channel.send(error));    
     })
 
