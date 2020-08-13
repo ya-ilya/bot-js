21775387bab1680e997c9531b55bb25f467b62c1
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-08-13 19:34:25 -0400

    Create historian.js

---
+++ commands/historian.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const fs = require("graceful-fs");
+
+module.exports.run = async (client, message, args) => {
+
+    message.channel.send(`I will eat you`);
+
+}
+
+module.exports.config = {
+    name: "historian",
+    aliases: [],
+    use: "historian [message]",
+    description: "he will eat you",
+    state: "gamma",
+    page: -1
+};
