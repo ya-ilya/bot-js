23b34ec83bf37d9748ccd3aecd8ad8db2d083c36
Dominika <sokolov.dominika@gmail.com>
2020-08-15 03:20:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -266,7 +266,7 @@ async function autoResponder(message) {
     /* any members with roles bypass the filter */
     if (!message.member.hasPermission("CHANGE_NICKNAME")) {
         /* current ongoing raid ban */
-        if (new RegExp("(nSwtv89|adpxqbC|kxRQ5cd)").test(message.content) || new RegExp("DDoS attack from Hydra Corporation").test(message.content)) {
+        if (new RegExp("(nSwtv89|adpxqbC|kxRQ5cd)").test(message.content) || new RegExp("ddos attack from hydra corporation").test(cleanedMessage)) {
             let embed = warnRule(message, "5, 9", "Automated ban for raiding and advertising. Contact a moderator (dominika#0076) if you think this was a mistake")
             let r = await message.author.send(embed);
             message.reply(embed);
