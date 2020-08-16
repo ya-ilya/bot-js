85c702131e7ff9f1070db967665a226c2b9e470a
TashDevCat <68488322+TashDevCat@users.noreply.github.com>
2020-08-16 00:09:42 -0400

    updated historian.js and xiaro.js (#162)
    * updated Xiaro
    
    made it delete the message, similar to ;say
    
    * Update historian.js
    
    made it delete the bot command
    
    Co-authored-by: TashNB <65786907+TashNB@users.noreply.github.com>

--- commands/historian.js
+++ commands/historian.js
@@ -4,7 +4,7 @@ const fs = require("graceful-fs");
 module.exports.run = async (client, message, args) => {
 
     message.channel.send(`I will eat you`);
-
+    message.delete()
 }
 
 module.exports.config = {
--- commands/xiaro.js
+++ commands/xiaro.js
@@ -3,6 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     message.channel.send(`Trans rights~! :3, ${message.content.slice(7)}`)
+    message.delete()
 }
 
 module.exports.config = {
