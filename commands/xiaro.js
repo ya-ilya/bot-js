528c94a413e137e2a7f87688b3b02eca18be6b08
Dominika <sokolov.dominika@gmail.com>
2020-08-24 19:42:01 -0400

    Update xiaro.js

--- commands/xiaro.js
+++ commands/xiaro.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
-    message.channel.send(`Trans rights~! :3, ${message.content.slice(7).replace(/[<@>/g, "")}`)
+    message.channel.send(`Trans rights~! :3, ${message.content.slice(7).replace(/[<@>]/g, "")}`)
 }
 
 module.exports.config = {
