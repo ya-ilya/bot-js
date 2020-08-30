7e81d0faf41495c172a328eff4421673225709a6
Dominika <sokolov.dominika@gmail.com>
2020-08-30 16:53:51 -0400

    Update faq.gui.js

--- commands/faq.gui.js
+++ commands/faq.gui.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) => {
     if ((message.channel.id == "724349410858893372") || (message.channel.id == "722436626248237076")) {
         message.channel.send("Вы можете уменьшить графический интерфейс, перейдя в «Настройки» -> «Настройки графики» и изменив масштаб графического интерфейса на «Обычный» или «Маленький».\n\nИспользуйте команду ;fixgui в игре, если после это всё рaвно у вас маленькое меню, или можете включить язык English (US)")
     } else {
-        message.channel.send("ClickGUI Settings -> Scale")
+        message.channel.send("ClickGUI Settings -> Scale\nMake sure you're using the latest version if you don't see this option.")
     }
 };
 
