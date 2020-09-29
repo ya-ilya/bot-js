ae74466996c752d4eafbc27c91d1fa5902f1348a
humboldt123 <38266782+humboldt123@users.noreply.github.com>
2020-09-29 13:14:31 -0400

    various optimizations to ping calculation

--- commands/ping.js
+++ commands/ping.js
@@ -3,7 +3,7 @@ const fs = require("graceful-fs");
 
 module.exports.run = async (client, message, args) => {
     const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp - 100}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 };
 
 module.exports.config = {
