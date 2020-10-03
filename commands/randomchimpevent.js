ead2ae4692c5eadbb77dd622388059a48e78aa74
kars0nn <64934094+kars0nn@users.noreply.github.com>
2020-10-03 14:19:45 -0400

    added monkeys (#191)

--- commands/randomchimpevent.js
+++ commands/randomchimpevent.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const fs = require("graceful-fs");
 module.exports.run = async (client, message, args) => {
 
-  const randomChimpEvents = [
+const randomChimpEvents = [
     "https://bigrat.monster/media/monkeys/CwQRLGK.mp4?.gif",
     "https://bigrat.monster/media/monkeys/Monkey_Orange.gif",
     "https://bigrat.monster/media/monkeys/ShallowUnhealthyDeermouse-size_restricted.gif",
@@ -17,7 +17,15 @@ module.exports.run = async (client, message, args) => {
     "https://bigrat.monster/media/monkeys/telephonemonkey.gif",
     "https://cdn.discordapp.com/attachments/634010567107149824/756703349541437469/video0.mp4",
     "https://cdn.discordapp.com/attachments/634510679465918474/761378483515293706/video0.mp4",
-    "https://cdn.discordapp.com/attachments/634510679465918474/761683469884981288/video0.mp4"
+    "https://cdn.discordapp.com/attachments/634510679465918474/761683469884981288/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005749588426762/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005767351566336/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005798041026640/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005826574745680/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005840240443402/video0.mov",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005845424734238/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762005728479150110/video0.mp4",
+    "https://cdn.discordapp.com/attachments/710891361615740948/762014906039271524/video0.mp4"
   ]; 
 
     message.channel.send(randomChimpEvents[Math.floor(Math.random() * randomChimpEvents.length)]);
