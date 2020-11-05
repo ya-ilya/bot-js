f19b1b457eb60b1db12c1674a59e659833388506
Dominika <sokolov.dominika@gmail.com>
2020-11-05 13:09:45 -0500

    I forgot to commit... So here you go.

--- main.js
+++ main.js
@@ -196,7 +196,7 @@ client.on('message', async message => {
 
     if (/cursedballs/.test(message.content)) message.delete()
 
-    if (message.content.includes(":transgender_flag:")) message.channel.send("Trans rights~:3 :transgender_flag:")
+    if (/trans.*flag/.test(message.content)) message.channel.send("Trans rights~:3 :transgender_flag:")
 
 });
 
