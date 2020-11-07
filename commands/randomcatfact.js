c1dead474c0417ea9ecfc31395d9a60613e712ac
DisNin <68169685+DisNin@users.noreply.github.com>
2020-11-08 02:18:47 +0300

    fix-images-random (#199)
    Co-authored-by: DISNIN_OWO <68169685+DSNN2@users.noreply.github.com>

--- commands/randomcatfact.js
+++ commands/randomcatfact.js
@@ -1,19 +1,23 @@
 const Discord = require("discord.js");
 const fs = require("graceful-fs");
-const cats = require('cat-ascii-faces')
+const cats = require('cat-ascii-faces');
 const catFacts = require('cat-facts');
-
+const symbols = 'qwertyuiopasdfghjk'
+function randomsymbols() {
+    return randomsymbols[Math.floor(Math.random() * symbols.length)]
+}
 /**
  * @module randomcatfact
  * @author sourTaste000
  */
 
+ 
 module.exports.run = async (client, message, args) => {
     let catEmbed = new Discord.MessageEmbed()
         .setColor(client.colors.kamiblue)
         .setTitle(catFacts.random())
         .setDescription(cats())
-        .setImage(`http://www.randomkittengenerator.com/cats/rotator.php`)
+        .setImage(`http://www.randomkittengenerator.com/cats/rotator.php`+"?"+`${randomsymbols()}`)
 
     message.channel.send(catEmbed);
 
